ef6849a0ee0011de7ee0a0cd20cb054e4f78e96f
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-01-23 13:20:47 +0200

    App

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,21 +7,21 @@ class ContactForm extends Component {
     onSubmit: PropTypes.func.isRequired,
   };
 
-  state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-    name: '',
-    number: ''
-  }
   // state = {
+  //   contacts: [
+  //     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  //     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  //     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  //     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  //   ],
+  //   filter: '',
   //   name: '',
-  //   number: '',
-  // };
+  //   number: ''
+  // }
+  state = {
+    name: '',
+    number: '',
+  };
 
   handleChange = (e) => {
     const { name, value } = e.currentTarget;
@@ -36,12 +36,7 @@ class ContactForm extends Component {
   };
 
   reset = () => {
-    this.setState({ contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ], filter: '', name: '', number: '' });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
